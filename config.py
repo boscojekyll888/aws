40fc6cb4da07bb418583fef2271fa35e08ef0e99
boscojekyll888 <124445016+boscojekyll888@users.noreply.github.com>
2023-04-29 16:15:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ckalfozkklug.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "wongrenjie-bucket"
+customregion = "us-east-1"
